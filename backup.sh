e64406eba17bec2140e69689c73345cba948d13e
monstars0 <monstars0@gmail.com>
2018-10-06 16:08:19 +0900

    ignore flyway

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 
 file=data.sql
 
-ssh monstars0@hunters.lisfactory.com mysqldump -u bbase -pdaisuke1 bbase --skip-opt --skip-dump-date --default-character-set=utf8 -t > $file
+ssh monstars0@hunters.lisfactory.com mysqldump -u bbase -pdaisuke1 bbase --skip-opt --skip-dump-date --default-character-set=utf8 -t --ignore-table=bbase.flyway_schema_history > $file
 diff=`git diff $file`
 
 if [ -n "$diff" ]; then

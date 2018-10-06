eda809ca342fa2a6afaaf8e763f8218c4d7a0b0f
monstars0 <monstars0@gmail.com>
2018-10-06 12:31:26 +0900

    update backup script so push changes to git

--- backup.sh
+++ backup.sh
@@ -1,1 +1,17 @@
-ssh monstars0@hunters.lisfactory.com mysqldump -u bbase -pdaisuke1 bbase --skip-opt --skip-dump-date --default-character-set=utf8 > dump.sql
+#!/bin/sh
+
+file=dump.sql
+
+ssh monstars0@hunters.lisfactory.com mysqldump -u bbase -pdaisuke1 bbase --skip-opt --skip-dump-date --default-character-set=utf8 > $file
+diff=`git diff $file`
+
+if [ -n "$diff" ]; then
+	datetime=`date "+%Y%m%d %H%M%S"`
+	message="update on $datetime"
+	git add $file
+	git commit -m $message
+	git push origin master
+	echo $message
+else
+	echo "no changes"
+fi
